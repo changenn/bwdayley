a93c1e4a0d359b6783310ea08a8aae573457508d
zhongxiaoping <zxp@sxw.cn>
2016-11-02 19:31:12 +0800

    first commit

---
+++ censortext.js
@@ -0,0 +1,26 @@
+
+
+var censoredWords = ["sad", "bad", "mad"];
+var customCensoreWords = [];
+
+function censor(inStr){
+    for(idx in censoredWords){
+        inStr = inStr.replace(censoredWords[idx], "*****");
+    }
+    for(idx in customCensoreWords){
+        inStr = inStr.replace(customCensoreWords[idx], "*****");
+    }
+    return inStr;
+}
+
+function addCensoredWord(word){
+    customCensoreWords.push(word);
+}
+
+function getCensoredWords(){
+    return censoredWords.concat(customCensoreWords);
+}
+
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords;  
